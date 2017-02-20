564bfefbfa23539ecbb9b35480200c155984a1b2
horike37 <horike37@gmail.com>
2017-02-20 23:00:53 +0900

    update

---
+++ bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+if [[ $BRANCH == 'master' ]]; then
+  STAGE="production"
+elif [[ $BRANCH == 'development' ]]; then
+  STAGE="development"
+fi
+if [ -z ${STAGE+x} ]; then
+  echo "Not deploying changes";
+  exit 0;
+fi
+echo "Deploying from branch $BRANCH to stage $STAGE"
+npm prune --production  #remove devDependencies
+sls deploy --stage $STAGE --region $AWS_REGION
